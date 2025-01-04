54dcc37d59e808c524fb28f7a6a05974e9855662
uf5 <notbailey13@gmail.com>
2025-01-04 13:09:59 +0500

    Refactor out the unused HM functions

--- src/iv/typing/inference.rs
+++ src/iv/typing/inference.rs
@@ -72,79 +72,6 @@ impl Typeable for OpType {
     }
 }
 
-#[derive(Clone)]
-struct Schema<T> {
-    pub vars: HashSet<String>,
-    pub t: T,
-}
-
-impl<T> Typeable for Schema<T>
-where
-    T: Typeable,
-{
-    fn ftv(&self) -> HashSet<String> {
-        let mut vs = self.t.ftv();
-        for v in self.vars.iter() {
-            vs.remove(v);
-        }
-        vs
-    }
-
-    fn apply(&self, subst: &Subst) -> Self {
-        let subst_no_vars = subst
-            .clone()
-            .into_iter()
-            .filter(|(k, _)| !self.vars.contains(k))
-            .collect();
-        Schema {
-            vars: self.vars.clone(),
-            t: self.t.apply(&subst_no_vars),
-        }
-    }
-}
-
-#[derive(Clone)]
-struct TypeEnv {
-    pub schemas: HashMap<String, Schema<Type>>,
-}
-
-impl Typeable for TypeEnv {
-    fn ftv(&self) -> HashSet<String> {
-        self.schemas.values().flat_map(Typeable::ftv).collect()
-    }
-
-    fn apply(&self, subst: &Subst) -> Self {
-        let schemas = self
-            .schemas
-            .iter()
-            .map(|(v, t)| (v.to_owned(), t.apply(subst)))
-            .collect();
-        TypeEnv { schemas }
-    }
-}
-
-impl TypeEnv {
-    pub fn new() -> Self {
-        TypeEnv {
-            schemas: HashMap::new(),
-        }
-    }
-
-    pub fn generalize<T>(&self, t: &T) -> Schema<T>
-    where
-        T: Typeable + Clone,
-    {
-        let vars = t.ftv().difference(&self.ftv()).cloned().collect();
-        Schema { vars, t: t.clone() }
-    }
-
-    pub fn remove(&self, v: &str) -> TypeEnv {
-        let mut new_env = self.clone();
-        new_env.schemas.remove(v);
-        new_env
-    }
-}
-
 pub struct Inference<'m> {
     pub module: &'m Module,
     counter: usize,
@@ -163,8 +90,7 @@ impl<'m> Inference<'m> {
             let Body::Body(ref body) = op_def.body else {
                 continue;
             };
-            let e = TypeEnv::new();
-            let (_, inf) = self.ti(&e, &body)?;
+            let (_, inf) = self.ti(&body)?;
             let s = self.unify_op(&op_def.ann, &inf)?;
             println!("OAMI:\nann:{:?}\ninf:{:?}\nsub:{:?}", &op_def.ann, &inf, &s);
             // let us assume that the ann matches the inf when all subs values are poly
@@ -185,15 +111,6 @@ impl<'m> Inference<'m> {
         let name = format!("gen_{}", self.counter);
         self.counter += 1;
         Type::Poly(name)
-    }
-
-    fn instantiate(&mut self, scm: &Schema<Type>) -> Type {
-        let Schema { vars, t } = scm;
-        let new_var_subst = vars
-            .iter()
-            .map(|v| (v.to_owned(), self.gen_name()))
-            .collect();
-        t.apply(&new_var_subst)
     }
 
     fn instantiate_op(&mut self, op: &OpType) -> OpType {
@@ -268,7 +185,7 @@ impl<'m> Inference<'m> {
         }
     }
 
-    fn ti_op(&mut self, e: &TypeEnv, op: &Op) -> Err<(Subst, OpType)> {
+    fn ti_op(&mut self, op: &Op) -> Err<(Subst, OpType)> {
         match op {
             Op::Literal(lit) => Ok((Subst::new(), self.ti_lit(lit))),
             Op::Name(n) => {
@@ -279,7 +196,7 @@ impl<'m> Inference<'m> {
                     .ok_or_else(|| InferenceError::UnknownOp(n.clone()))?;
                 Ok((Subst::new(), self.instantiate_op(&op_def.ann)))
             }
-            Op::Case(hash_map) => todo!(),
+            Op::Case(_arms) => todo!(),
         }
     }
 
@@ -345,13 +262,12 @@ impl<'m> Inference<'m> {
         }
     }
 
-    fn ti(&mut self, e: &TypeEnv, ops: &[Op]) -> Err<(Subst, OpType)> {
+    fn ti(&mut self, ops: &[Op]) -> Err<(Subst, OpType)> {
         match ops {
             [] => Ok((Subst::new(), OpType::empty())),
             [o, os @ ..] => {
-                let (s1, t1) = self.ti_op(e, o)?;
-                let e1 = e.apply(&s1);
-                let (s2, t2) = self.ti(&e1, os)?;
+                let (s1, t1) = self.ti_op(o)?;
+                let (s2, t2) = self.ti(os)?;
                 let (s3, t3) = self.chain(&t1, &t2)?;
                 let s = compose(&s3, &compose(&s2, &s1));
                 let t = t3.apply(&s);
